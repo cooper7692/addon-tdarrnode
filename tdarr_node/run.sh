74a119da2c780d71c77cc26bde2e30dd239b6bff
cooper7692 <Cooper7692@gmail.com>
2025-05-21 06:19:35 -0400

    added the files to the repo
    added the files to the repo

---
+++ tdarr_node/run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+export Tdarr_ServerIP="${TDARR_SERVER_IP}"
+export Tdarr_ServerPort="${TDARR_SERVER_PORT}"
+export Tdarr_NodeID="${NODE_ID}"
+
+cd /app/tdarr_node
+
+./Tdarr_Node
