8e504573c06cb95b728fc7c7babdcc5a537819ac
cooper7692 <Cooper7692@gmail.com>
2025-05-23 17:40:02 -0400

    added starting configs, and folder  structure and icon  and modified  a run cmds and config  more tweaks coming after testing and a version bum to follow once working

--- tdarrnode/run.sh
+++ tdarrnode/run.sh
@@ -1,26 +1,12 @@
 #!/usr/bin/env bash
 
+cd /tdarr || exit 1
+echo "[\$(date '+%Y-%m-%d %H:%M:%S')] Changed working directory to /tdarr"
+
 echo "[\$(date '+%Y-%m-%d %H:%M:%S')] Starting Tdarr Node Updater..."
-/tdarr/Tdarr_Updater
+exec ./Tdarr_Updater
 
 echo "[\$(date '+%Y-%m-%d %H:%M:%S')] Waiting for node to finish setup..."
-sleep 10
+sleep 60
 
-echo "[\$(date '+%Y-%m-%d %H:%M:%S')] Creating config file..."
-cat <<EOF > /config/Tdarr_Node_Config.json
-{
-  "nodeID": "${NODEID}",
-  "serverIP": "${SERVERIP}",
-  "serverPort": "${SERVERPORT}",
-  "nodeIP": "0.0.0.0",
-  "nodePort": "8267",
-  "ffmpegPath": "",
-  "handBrakePath": "",
-  "nodeTempPath": "/temp"
-}
-EOF
-
-echo "[\$(date '+%Y-%m-%d %H:%M:%S')] Launching Tdarr Node with configuration:"
-cat /config/Tdarr_Node_Config.json
-
-exec /tdarr/Tdarr_Node
+exec ./Tdarr_Node
